0a12a578435acdbd25107c47d080ff30fc3608e5
LuisDavid01 <luisdavidmirandav12@gmail.com>
2026-02-23 10:12:42 -0600

    feat: admin por defecto en seed

--- Server/prisma/seed.ts
+++ Server/prisma/seed.ts
@@ -6,22 +6,43 @@ async function main() {
 	await prisma.role.createMany({
 		data: [
 			{
+				ID: 1,
 				Name: "admin",
 			},
 			{
+				ID: 2,
 				Name: "bartender",
 			},
 			{
+				ID: 3,
 				Name: "chef",
 			},
 			{
+				ID: 4,
 				Name: "mesero",
 			},
 			{
+				ID: 5,
 				Name: "soporte",
 			},
 		],
+		skipDuplicates: true,
 	});
+
+	await prisma.usuario.create({
+		data: {
+			Email: "vrtxai@protonmail.com",
+			Password: "$2b$10$g0MtRwH3c8vjdfGWXdrk3eaHymEdi7BMwl./ZcTGfJ3xbxMMib5V.",
+			Nombre: "admin",
+			Apellido: "admin",
+			Cedula: "123456789",
+			Roles: {
+				connect: [{
+					ID: 1
+				}]
+			},
+		},
+	})
 }
 
 main()
