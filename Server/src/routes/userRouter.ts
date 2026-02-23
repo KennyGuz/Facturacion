01f12f697cce039388916fa4a760f4dd4aadb558
LuisDavid01 <luisdavidmirandav12@gmail.com>
2026-02-23 10:16:29 -0600

    feat: forgot about this too :v

--- Server/src/routes/userRouter.ts
+++ Server/src/routes/userRouter.ts
@@ -4,7 +4,7 @@ import { userController } from "@/controllers/userController";
 
 const router = Router();
 
-router.get("/user/:id", verifyToken, validateRol('admin'), userController.getUser);
+router.get("/user/:id",  verifyToken, validateRol('admin'), userController.getUser);
 router.get("/users", verifyToken, validateRol('admin'), userController.getUsers);
 router.put("/user/:id", verifyToken, validateRol('admin'), userController.updateUser);
 router.delete("/user/:id", verifyToken, validateRol('admin'), userController.deleteUser);
